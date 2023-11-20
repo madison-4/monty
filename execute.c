cacc1270fbd1071742eee41d0ebca264e24b8280
nox <samingosi97@gmail.com>
2023-11-19 19:08:16 -0800

    return monty global variable

--- execute.c
+++ execute.c
@@ -3,10 +3,10 @@
 monty_s monty = {NULL, NULL, NULL, 0};
 
 /**
- *main - main function
- *@argc: argument count
- *@argv: argument vector
- *Return: 0 on success
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
